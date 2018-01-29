c08f536ccfe17acd14dd30df4e38c0430a5cb3c2
Christina71 <mariachristina71@yahoo.com>
2018-01-29 11:09:43 -0500

    Initial commit

---
+++ src/main/java/com/example/fibo/FiboApplication.java
@@ -0,0 +1,12 @@
+package com.example.fibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiboApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/fibo/FiboNacci.java
@@ -0,0 +1,26 @@
+package com.example.fibo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FiboNacci {
+    static int num1,num2,num3,num4,num5,num6,num7,num8;
+    @RequestMapping("/fibb")
+    public String showNum( @RequestParam("num") int num1)
+    {
+
+        String output ="";
+        num2=num1+1;
+        num3=num2+num1;
+        num4=num3+num2;
+        num5=num4+num3;
+        num6=num5+num4;
+        num7=num6+num5;
+        num8=num7+num6;
+
+        output = output + " " + num1 +" " +num2 + " " + num3 + " "  + num4 + " " + num5 + " " + num6 + " " + num7 + " " + num8;
+        return output;
+    }
+}
